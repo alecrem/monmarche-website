619e4c5213ef7c31235551e541ee82cbaed7283c
Alejandro Cremades <ale@alecrem.com>
2023-11-11 12:41:41 +0900

    Update Ogori shop address

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ const Index = () => (
           >
             <ShopCard
               title={'mon Marché'}
-              address={'838-0124 福岡県小郡市古飯489-1'}
+              address={'838-0124 福岡県小郡市古飯789-1'}
               openingHours={'水・金曜日+たまの週末 11:00-16:00'}
               phone={'090-2078-7809'}
               email={'info@mmfukuoka.com'}
